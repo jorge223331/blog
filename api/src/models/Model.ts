57d4392bba75d933d6f0129795aa766a3855a67f
george.parshin <george.parshin@servermania.com>
2023-04-18 02:33:31 +0400

    upd

---
+++ api/src/models/Model.ts
@@ -0,0 +1,33 @@
+
+
+export interface IModel {
+  id: number;
+  created: number;
+  updated: number;
+}
+export class Model<Type extends IModel> {
+  id!: number;
+  created!: number;
+  updated!: number;
+  constructor(data: Type) {
+    const self = this as any;
+    for (const k in data) {
+      if (Object.prototype.hasOwnProperty.call(data, k)) {
+        self[k] = data[k];
+      }
+    }
+  }
+}
+export class ModelInsertable<Type extends Record<string, any>> {
+  id!: number;
+  created!: number;
+  updated!: number;
+  constructor(data: Type) {
+    const self = this as any;
+    for (const k in data) {
+      if (Object.prototype.hasOwnProperty.call(data, k)) {
+        self[k] = data[k];
+      }
+    }
+  }
+}
