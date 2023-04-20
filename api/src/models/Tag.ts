80963df075263229c5e196095cda6465c993eddd
george.parshin <george.parshin@servermania.com>
2023-04-20 20:04:04 +0400

    upd

--- api/src/models/Tag.ts
+++ api/src/models/Tag.ts
@@ -1,14 +1,14 @@
 import { IModel, Model, ModelInsertable } from "./Model.js";
 
-export interface Tag {
+export interface ITagInsertable {
   name: string;
   slug: string;
 }
-export class TagInsertable extends ModelInsertable<TagInsertable> {
+export class TagInsertable extends ModelInsertable<ITagInsertable> {
   name!: string;
   slug!: string;
 }
-export interface ITag extends Tag, IModel {}
+export interface ITag extends ITagInsertable, IModel {}
 export class Tag extends Model<ITag> {
   name!: string;
   slug!: string;
