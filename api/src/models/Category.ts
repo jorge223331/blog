96968fa27000b727f25f8c1bd13cfa95d335c660
george.parshin <george.parshin@servermania.com>
2023-04-24 23:06:26 +0400

    upd

--- api/src/models/Category.ts
+++ api/src/models/Category.ts
@@ -16,27 +16,48 @@ export class Category extends Model<ICategory> {
   link!: string;
   slug!: string;
 }
-interface Table {
-  query: string;
+export async function createCategory(c: CategoryInsertable) {
+  const created = new Date();
+  const querry = {
+    text: "INSERT INTO categories (name, link, slug, created) VALUES ($1, $2, $3, $4)",
+    values: [c.name, c.link, c.slug, created],
+  };
+  client
+    .query(querry)
+    .then(() => {
+      console.log(`Category ${c.name} created`);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 }
-const tables: Table[] = [
-  {
-    query: `CREATE TABLE IF NOT EXISTS categories (
-  id SERIAL PRIMARY KEY,
-  name NOT NULL,
-  link NOT NULL,
-  slug NOT NULL
-  )`,
-  },
-];
-async function createTable(table: Table) {
-  try {
-    await client.query(table.query);
-    console.log("Table created");
-  } catch (err) {
-    console.log("error", err);
-  }
+export async function updateCategory(c: CategoryInsertable, id: number) {
+  const updated = new Date();
+  const querry = {
+    text: "UPDATE categories SET name = $2, link = $3 slug = $4, updated = $5, WHERE id = $6)",
+    values: [c.name, c.link, c.slug, updated, id],
+  };
+  client
+    .query(querry)
+    .then(() => {
+      console.log(`Category #${id} updated`);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 }
-export function callTable() {
-  tables.map((t) => createTable(t));
+export async function deleteCategory(d: CategoryInsertable, id: number) {
+  const deleted = new Date();
+  const querry = {
+    text: "DELETE FROM categories WHERE id = $1",
+    values: [id],
+  };
+  client
+    .query(querry)
+    .then(() => {
+      console.log(`Category #${id} deleted`);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 }
